2862b64108bda98acdca2431df9d7f3fa3b35833
Tuấn Lương <ductuan0112@gmail.com>
2026-06-21 11:54:13 +0700

    Add files via upload

---
+++ Session10/exercise02/exercise02.js
@@ -0,0 +1,14 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Ha Noi";
+
+document.write(i);
+document.write(f);
+document.write(b);
+document.write(s);
+
+console.log(i);
+console.log(f);
+console.log(b);
+console.log(s);
