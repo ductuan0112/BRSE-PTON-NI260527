68de54240e4d8d7a1f5fadcd0362354fe8196978
Tuấn Lương <ductuan0112@gmail.com>
2026-06-21 12:17:07 +0700

    Update exercise04.js

--- Session10/exercise04/exercise04.js
+++ Session10/exercise04/exercise04.js
@@ -1,4 +1,4 @@
 let firstNumber = Number(prompt('Nhập vào số thứ nhất:'));
 let secondNumber = Number(prompt('Nhập vào số thứ hai:'));
 
-alert(`Tổng hai số là: ${firstNumber + secondNumber}`)
+alert(`Tổng hai số là: ${firstNumber + secondNumber}`);
