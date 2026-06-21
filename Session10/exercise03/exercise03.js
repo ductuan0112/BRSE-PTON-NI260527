58d4a57215cddd75a07855187bcb0f76278173de
Tuấn Lương <ductuan0112@gmail.com>
2026-06-21 12:06:15 +0700

    Add files via upload

---
+++ Session10/exercise03/exercise03.js
@@ -0,0 +1,7 @@
+let width = Number(prompt('Nhập vào giá trị chiều rộng:'));
+let height = Number(prompt('Nhập vào giá trị chiều dài:'));
+
+let area = width * height;
+
+// document.write(`Diện tích là: ${area}`);
+console.log(`Diện tích là: ${area}`)
